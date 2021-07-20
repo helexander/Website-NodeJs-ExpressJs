a027552985339084e719b6acfe710be1ea4d0296
helexander <jeremychee97@gmail.com>
2021-07-20 15:06:17 +1000

    Added feedback form and POST function

--- routes/feedback.js
+++ routes/feedback.js
@@ -8,13 +8,16 @@ module.exports = (params) => {
     router.get('/', async (req, res, next) => {
         try {
             const feedback = await feedbackService.getList();
-            return res.json(feedback);
+            return res.render('layout', { pageTitle: 'Feedback', template: 'feedback', feedback });
         } catch (err) {
             return next(err);
         }
     });
 
-    router.post('/', (req, res) => res.send('Feedback form posted'));
+    router.post('/', (req, res) => {
+        console.log(req.body);
+        res.send('Feedback form posted')
+    });
 
     return router;
 };
