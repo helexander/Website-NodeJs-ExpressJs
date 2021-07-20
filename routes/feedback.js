cbacd87b499681a82ee154ff23235be3412e3288
helexander <jeremychee97@gmail.com>
2021-07-20 16:02:14 +1000

    Removal of redundant script, pixgrid, when not needed to reduce loading time

--- routes/feedback.js
+++ routes/feedback.js
@@ -4,6 +4,29 @@ const { check, validationResult } = require('express-validator');
 
 const router = express.Router();
 
+const validations = [
+    check('name')
+        .trim()
+        .isLength({ min: 3 })
+        .escape()
+        .withMessage('A name is required'),
+    check('email')
+        .trim()
+        .isEmail()
+        .normalizeEmail()
+        .withMessage('A valid email address is required'),
+    check('title')
+        .trim()
+        .isLength({ min: 3 })
+        .escape()
+        .withMessage('A title is required'),
+    check('name')
+        .trim()
+        .isLength({ min: 3 })
+        .escape()
+        .withMessage('A message is required'),
+];
+
 module.exports = (params) => {
     const { feedbackService } = params;
 
@@ -12,45 +35,55 @@ module.exports = (params) => {
             const feedback = await feedbackService.getList();
 
             const errors = req.session.feedback ? req.session.feedback.errors : false;
+
+            const successMessage = req.session.feedback ? req.session.feedback.message : false;
+
+
             req.session.feedback = {};
 
-            return res.render('layout', { pageTitle: 'Feedback', template: 'feedback', feedback, errors });
+            return res.render('layout', { pageTitle: 'Feedback', template: 'feedback', feedback, errors, successMessage });
         } catch (err) {
             return next(err);
         }
     });
 
-    router.post('/', [
-        check('name')
-            .trim()
-            .isLength({ min: 3 })
-            .escape()
-            .withMessage('A name is required'),
-        check('email')
-            .trim()
-            .isEmail()
-            .normalizeEmail()
-            .withMessage('A valid email address is required'),
-        check('title')
-            .trim()
-            .isLength({ min: 3 })
-            .escape()
-            .withMessage('A title is required'),
-        check('name')
-            .trim()
-            .isLength({ min: 3 })
-            .escape()
-            .withMessage('A message is required'),
-    ], (req, res) => {
-        const errors = validationResult(req);
+    router.post('/', validations, async (req, res, next) => {
+        try {
+            const errors = validationResult(req);
 
-        if (!errors.isEmpty()) {
+            if (!errors.isEmpty()) {
+                req.session.feedback = {
+                    errors: errors.array(),
+                };
+                return res.redirect('/feedback');
+            }
+
+            const { name, email, title, message } = req.body;
+            await feedbackService.addEntry(name, email, title, message);
             req.session.feedback = {
-                errors: errors.array(),
-            };
+                message: 'Thank you for your feedback',
+            }
+
             return res.redirect('/feedback');
+        } catch (err) {
+            return next(err);
         }
-        return res.send('Feedback form posted');
+
+    });
+
+    router.post('/api', validations, async (req, res, next) => {
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.json({ errors: errors.array() });
+            }
+            const { name, email, title, message } = req.body;
+            await feedbackService.addEntry(name, email, title, message);
+            const feedback = await feedbackService.getList();
+            return res.json({ feedback });
+        } catch (err) {
+            return next(err);
+        }
     });
 
     return router;
